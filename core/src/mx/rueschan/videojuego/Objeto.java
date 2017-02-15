7b97d90f90712ea74d7f266b4e4cd6b962808e4e
Jonathan <A01377844@itesm.mx>
2017-02-15 09:50:03 -0600

    Superclase Objeto agregada/ LetreroPlay más oscuro

---
+++ core/src/mx/rueschan/videojuego/Objeto.java
@@ -0,0 +1,22 @@
+package mx.rueschan.videojuego;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Jonathan on 15/02/2017.
+ */
+
+public class Objeto {
+    protected Sprite sprite;
+
+    public Objeto(Texture textura, float x, float y){
+        sprite = new Sprite(textura);
+        sprite.setPosition(x,y);
+    }
+
+    public void dibujar(SpriteBatch batch){
+        sprite.draw(batch);
+    }
+}
