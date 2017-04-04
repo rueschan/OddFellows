d578dac815d1fd162fce3e12c8e0d41f57120082
Jonathan <A01377844@itesm.mx>
2017-04-04 13:45:01 -0500

    Creación de clase Enemigo //Diferentes tonos de bosque agregados

---
+++ core/src/mx/rueschan/videojuego/Enemigo.java
@@ -0,0 +1,19 @@
+package mx.rueschan.videojuego;
+
+/**
+ * Created by OddFellows on 04/04/2017.
+ */
+
+public class Enemigo extends Objeto {
+
+    //Método que se acercará al personaje más no indicará como atacar
+    private void Perseguir(Personaje jugador){
+
+    }
+
+    //Método que determinará si se hará un ataque al personaje y realizará el daño correspondiente
+    private void Atacar(Personaje jugador){
+
+    }
+
+}
